d31bb03fb3973006f60d2f3e5455554f10be2c9d
HHrxxx <rouxinghuang@163.com>
2020-01-24 03:52:10 +0800

    update

--- chinaTaxFilePy.py
+++ chinaTaxFilePy.py
@@ -4,6 +4,7 @@
 import requests
 from bs4 import BeautifulSoup
 import pymysql
+import datetime
 
 # 建立数据库连接
 db_conn = pymysql.connect(host="localhost", user="root", password="root", db="taxspider", charset="utf8")
@@ -12,50 +13,53 @@ cur = db_conn.cursor();
 
 hdrs = {
     'User-Agent':
-        'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E)'
+        'Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 '
 }
-url = "http://www.chinatax.gov.cn/chinatax/whmanuscriptList/n810755?_channelName=%E6%9C%80%E6%96%B0%E6%96%87%E4%BB%B6&_isAgg=0&_pageSize=20&_template=index"
+
+# for i in range(2, 50):
+# url = "http://www.chinatax.gov.cn/chinatax/manuscriptList/n8107" \
+#       "24?_isAgg=0&_pageSize=20&_template=index&_channelName=%E7%A8%8E%E5%8A%A1%E6%96%B0%E9%97%BB&_keyWH=wenhao&page={}".format(i)
+# url = "http://www.chinatax.gov.cn/chinatax/whmanuscriptList/n810755?_isAgg=0&_pageSize=20&_template=index&_channelName=%E6%9C%80%E6%96%B0%E6%96%87%E4%BB%B6&_keyWH=wenhao&page={}".format(
+#     i)
+url = "http://www.chinatax.gov.cn/chinatax/whmanuscriptList/n810755?_isAgg=0&_pageSize=20&_template=index&_channelName=%E6%9C%80%E6%96%B0%E6%96%87%E4%BB%B6&_keyWH=wenhao&page=3"
 r = requests.get(url, headers=hdrs)
 soup = BeautifulSoup(r.content.decode('utf8', 'ignore'), 'html.parser')
-
 # 获取指定class下的ul
 ul_list = soup.find('ul', class_='list')
-# 获取所有的a标签
-href_list = ul_list.find_all('a')
-# 获取所有span标签
-span_list = ul_list.find_all('span')
-
-count = 0
-for item in ul_list:
-    hre = href_list[count]['href']  # 链接
-    source = span_list[count].contents[0]  # 来源
-    title = href_list[count].contents[0]  # 标题
-    count = count + 1
-
-    #获取content，postTime
+if (ul_list is None):
+    print("None")
+# 获取所有的li标签
+li_list = ul_list.find_all('li')
+print(url)
+for item in li_list:
+    k = item.find_all('a')[0]  # 获取a标签
+    # hre = ur+k.get('href')  # 链接
+    hre = k.get('href')  # 链接
+    title = k.contents[0]  # 标题
+    source = item.find_all('span')[0].contents[0]
+    # source = "国家税务总局"
+    # 获取content
     r = requests.get(hre, headers=hdrs)
     soup2 = BeautifulSoup(r.content.decode('utf8', 'ignore'), 'html.parser')
-    div_list = soup2.find_all('div', class_='text')
+    div_list2 = soup2.find_all('div', class_='text')
     content = ""
     postTime = ""
-    if(len(div_list)>=1):
-        contentAll = div_list[1]  # fontzoom
+    if (len(div_list2) != 0):
+        contentAll = div_list2[1]  # fontzoom
         p_list = contentAll.find_all('p')
-        length=len(p_list)
+
         cnt = 0
         for i in p_list:
-            c=i.text
-            if (cnt==(length-2)):
-                postTime=c
-            if (cnt == (length - 1)):
-                break
-            content = content + c
-            cnt+=1
+            content += i.text
+            if (cnt == (len(p_list) - 2)):
+                postTime = i.text  # 只有在文章内容中才有时间，不一定能爬取到，每个文章排版位置都不一样。
 
-    data = (title, hre, source, content,postTime,0)
+    data = (title, hre, source, content, postTime, 0, datetime.datetime.now())  # Date记录爬取时间
     try:
-        cur.execute("insert into china(Title,Url,Source,Content,PostTime,Status) values('%s','%s','%s','%s','%s','%s')" % (data))
-        # 事物提交
+        cur.execute(
+            "insert into china(Title,Url,Source,Content,PostTime,Status,Date) values('%s','%s','%s','%s','%s','%s','%s')" % (
+                data))
+
         db_conn.commit()
     except Exception as err:
         print("sql语句执行错误", err)
--- chinaTaxNewsPy.py
+++ chinaTaxNewsPy.py
@@ -10,25 +10,31 @@ db_conn = pymysql.connect(host="localhost", user="root", password="root", db="ta
 # 创建游标对象
 cur = db_conn.cursor();
 
-hdrs = {'User-Agent': '(KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3'}
+hdrs = {'User-Agent': 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E)'}
 
-for i in range(1, 10):
-    url = "http://www.chinatax.gov.cn/chinatax/manuscriptList/n810724?_isAgg=0&_pageSize=20&_template=index&_channelNam" \
-          "e=%E7%A8%8E%E5%8A%A1%E6%96%B0%E9%97%BB&_keyWH=wenhao&page={}".format(i)
+for i in range(1,50):
+    # url = "http://www.chinatax.gov.cn/chinatax/manuscriptList/n8107" \
+    #       "24?_isAgg=0&_pageSize=20&_template=index&_channelName=%E7%A8%8E%E5%8A%A1%E6%96%B0%E9%97%BB&_keyWH=wenhao&page={}".format(i)
+    url = "http://www.chinatax.gov.cn/chinatax/manuscriptList/n810724?_isAgg=0&_pageSize=20&_template=index&_channelName=税务新闻&_keyWH=wenhao&page={}".format(i)
+    ur = "http://www.chinatax.gov.cn"  # 拼接
     r = requests.get(url, headers=hdrs)
     soup = BeautifulSoup(r.content.decode('utf8', 'ignore'), 'html.parser')
 
     # 获取指定class下的ul
     ul_list = soup.find('ul', class_='list')
+    if (ul_list is None):
+        print("None")
+        continue
     # 获取所有的li标签
     li_list = ul_list.find_all('li')
-
+    print(i)
     for item in li_list:
         k = item.find_all('a')[0]  # 获取a标签
+        # hre = ur+k.get('href')  # 链接
         hre = k.get('href')  # 链接
         title = k.contents[0]  # 标题
-        postTime = item.find_all('span')[0].contents[0]
-        source="国家税务总局"
+        postTime = item.find_all('span')[0].contents[0][1:-1]
+        source = "国家税务总局"
         # 获取content
         r = requests.get(hre, headers=hdrs)
         soup2 = BeautifulSoup(r.content.decode('utf8', 'ignore'), 'html.parser')
@@ -40,7 +46,7 @@ for i in range(1, 10):
         p_list = contentAll.find_all('p')
 
         for i in p_list:
-            content = content+i.text
+            content = content + i.text
 
         data = (title, hre, source, content, postTime, 0)
         try:
